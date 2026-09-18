be5ba343239c524992e3a3dbfbc6ef6acec9c731
Sayok Biswas <sayokbiswas538@gmail.com>
2026-09-18 09:40:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (84.37%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,21 +1,41 @@
 
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int>& digits) {
+//         vector<int> result;
+//         int carry = 1; 
+
+//         for (int i = digits.size() - 1; i >= 0; i--) {
+//             int sum = digits[i] + carry;
+//             result.push_back(sum % 10);
+//             carry = sum / 10;
+//         }
+//         if (carry > 0) {
+//             result.push_back(carry);
+//         }
+        
+            
+//         }
+//         reverse(result.begin(), result.end());
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> result;
-        int carry = 1; 
-
-        for (int i = digits.size() - 1; i >= 0; i--) {
-            int sum = digits[i] + carry;
-            result.push_back(sum % 10);
-            carry = sum / 10;
+        int n=digits.size();
+        for(int i=n-1;i>=0;i--)
+        {
+            if(digits[i]<9)
+            {
+                digits[i]=digits[i]+1;
+                return digits;
+            }
+            digits[i]=0;
         }
-
-        if (carry > 0) {
-            result.push_back(carry);
-        }
-        
-        reverse(result.begin(), result.end());
-        return result;
+        digits.push_back(1);
+        reverse(digits.begin(),digits.end());
+        return digits;
     }
 };
