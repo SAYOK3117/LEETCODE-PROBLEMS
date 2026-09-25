6d0a3ea89a3046943aaaea1d9f3d594b6755eac4
Sayok Biswas <sayokbiswas538@gmail.com>
2026-09-25 12:27:09 +0530

    Time: 61 ms (31.42%) | Memory: 28.7 MB (12.31%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,16 +9,54 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *p=headA;
-        ListNode *q=headB;
-        while(p!=NULL){
-            ListNode* temp = q;
-            while(temp!=NULL){
-                if(p==temp) return temp;
-                temp = temp->next;
-            }
-            p=p->next;
+        // Brute force
+        // ListNode *p=headA;
+        // ListNode *q=headB;
+        // while(p!=NULL){
+        //     ListNode* temp = q;
+        //     while(temp!=NULL){
+        //         if(p==temp) return temp;
+        //         temp = temp->next;
+        //     }
+        //     p=p->next;
+        // }
+        // return NULL;
+
+
+        //set ds 
+        unordered_set<ListNode*> seen;
+        ListNode *p = headA;
+        while(p){
+                seen.insert(p);
+            p = p->next;
+        }
+        p = headB;
+        while (p) {
+            if (seen.count(p)) return p;
+            p = p->next;
         }
         return NULL;
+
+
+
+
+
+
+
+
+        // set ds
+        // unordered_set<ListNode*> seen;
+        // ListNode *p = headA;
+        // while (p) {
+        //     visited.insert(p);
+        //     p = p->next;
+        // }
+        // p = headB;
+        // while (p) {
+        //     if (visited.count(p)) return p;
+        //     p = p->next;
+        // }
+        // return NULL;
+
     }
 };
